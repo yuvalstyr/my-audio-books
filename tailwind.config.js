cf71692e572f80754c4b8f76fa001cef466144ac
yuvalstyr <yuvalstyr@gmail.com>
2025-09-13 18:05:46 +0300

    revert

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
-    plugins: [require('daisyui')],
-    daisyui: {
-        themes: ['dim'],
-        darkTheme: 'dim',
-    }
 };
